96df79aad67721420882a84a445b2a3f7169328f
yondemon <pablo.rod@amorodio.es>
2019-02-06 12:19:47 +0100

    New providers: USA + fandor + tubi-tv + kanopy + hoopla + syfy

--- main.js
+++ main.js
@@ -583,6 +583,7 @@ var providers = {
   15: 'hulu',
   18: 'playstation',
   19: 'netmovies', // BR
+  25: 'fandor',
   27: 'hbo-now', // USA
   31: 'hbo-go', // USA
   34: 'epix',
@@ -590,11 +591,13 @@ var providers = {
   //37: 'showtime', // USA
   47: 'looke', // BR
   52: 'cravetv', // CA
+  60: 'fandangonow',
   62: 'atres-player',
   63: 'filmin',
   64: 'filmin-plus',
   67: 'blim', // MX
   68: 'microsoft-store',
+  73: 'tubi-tv',
   //81: 'CINEMA',
   84: 'u-next', // JP
   85: 'dtv', // JP
@@ -610,7 +613,10 @@ var providers = {
   169: 'the-movie-network-go', // CA
   182: 'hollywood-suite', // CA
   188: 'youtube-red',
+  191: 'kanopy', // USA
   192: 'youtube',
+  212: 'hoopla', // USA
+  215: 'syfy', // USA
 }
 
 var price = {
