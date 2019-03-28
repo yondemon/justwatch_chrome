921d34fbba0a1c8691192ce1853ba3a1127596f9
yondemon <pablo.rod@amorodio.es>
2019-03-28 10:00:52 +0100

    Refactor in multiple files Cleared debug log messages

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log("JustWatch!");
 const browser = window.browser || window.chrome;
 
-var debug = true;
+var debug = false;
 var l18n = 'es_ES';
 const API_DOMAIN = 'apis.justwatch.com';
 const DOMAIN = 'justwatch.com';
@@ -235,9 +235,8 @@ class JustWatchChrome {
               div.appendChild( this.getRatingsHTML(item.scoring) );
               div.appendChild( this.getOffersHTML(item.offers) );
 
-              this.movieJustWatchData = this.getTitle(this.type, item.id);
-
-              if(debug) console.log({data:this.movieJustWatchData});
+              // this.movieJustWatchData = this.getTitle(this.type, item.id);
+              // if(debug) console.log({data:this.movieJustWatchData});
             }
             done = true;
 
@@ -268,6 +267,7 @@ class JustWatchChrome {
 
   async getTitle(content_type, title_id)
   {
+    /*
     if(debug) console.log('getTitle',{content_type, title_id})
     title_id = encodeURIComponent(title_id);
     content_type = encodeURIComponent(content_type);
@@ -285,6 +285,7 @@ class JustWatchChrome {
         return xhr.responseText;
       }
     }
+    */
   }
 
   getPanelTitleHTML(){
@@ -310,13 +311,6 @@ class JustWatchChrome {
 
   removeNoMatches(){
     this.noMatchesP.parentNode.removeChild(this.noMatchesP);
-  }
-
-  setPresentationTypesMenu(presentationTypes){
-    console.log(presentationTypes);
-
-    var titlepanel = document.getElementById('justwatch-title');
-    //presentationTypes.each();
   }
 
   setCheapestOffer(offer,offersDiv,offersData){
@@ -345,20 +339,9 @@ class JustWatchChrome {
     var ratingsUl = document.createElement("ul");
     ratingsDiv.appendChild( ratingsUl );
 
-    var ratingSites = {
-      'imdb': {
-        'url': 'https://www.imdb.com/title/',
-      },
-      'tmdb': {
-        'url': 'https://www.themoviedb.org/movie/',
-      },
-      'tomato': {
-        'url': 'https://www.rottentomatoes.com/m/',
-      }
-    }
-
     let cleanedRatings = {};
-    console.log({ratings});
+    if(debug) console.log({ratings});
+
     for (var rating of ratings) {
       let [provider] = rating.provider_type.split(':');
       switch(rating.provider_type){
@@ -382,7 +365,7 @@ class JustWatchChrome {
           break;
       }
     }
-    console.log({cleanedRatings});
+    if(debug) console.log({cleanedRatings});
 
     for (const [provider, content] of Object.entries(cleanedRatings) ){
       let ratingStr = '';
@@ -417,15 +400,7 @@ class JustWatchChrome {
     var offersDiv = document.createElement("div");
     offersDiv.classList.add('justwatch-offers');
 
-    var lists = {
-      'flatrate': 'Flat',
-      'rent': 'Rent',
-      'buy': 'Buy',
-      'free': 'Free',
-      'cinema': 'Cinema',
-      'other': '-',
-      },
-      presentationTypes = {
+    var presentationTypes = {
         //'cheapest': false,
         'sd': false,
         'hd': false,
@@ -436,13 +411,13 @@ class JustWatchChrome {
 
     var ulBlocks = {};
 
-    for (var dataList in lists) {
-      if (lists.hasOwnProperty(dataList)) {
+    for (var dataList in providerLists) {
+      if (providerLists.hasOwnProperty(dataList)) {
         offersData[dataList] = {};
 
         ulBlocks[dataList] = document.createElement("ul");
         ulBlocks[dataList].setAttribute('data-monetization_type',dataList);
-        ulBlocks[dataList].setAttribute('data-title',lists[dataList]);
+        ulBlocks[dataList].setAttribute('data-title',providerLists[dataList]);
         offersDiv.appendChild( ulBlocks[dataList] );
       }
     }
@@ -502,7 +477,7 @@ class JustWatchChrome {
                 +    ' provider-'+offer.provider_id+' '+(cheapest?'cheapest':'')+'"><a href="' + url
                 + '"><span class="provider provider-'+offer.provider_id+'">'+logo+'</span>  <span class="presentation">' 
                 + offer.presentation_type + '</span>  <span class="price">' 
-                + offer.retail_price + ' ' + price[offer.currency] + '</span></a></li>\n');
+                + offer.retail_price + ' ' + currency[offer.currency] + '</span></a></li>\n');
               break;
 
           case 'buy':
@@ -519,7 +494,7 @@ class JustWatchChrome {
                 +    ' provider-'+offer.provider_id+' '+(cheapest?'cheapest':'')+'"><a href="' + url
                 + '"><span class="provider provider-'+offer.provider_id+'">'+logo+'</span>  <span class="presentation">' 
                 + offer.presentation_type + '</span>  <span class="price">' 
-                + offer.retail_price + ' ' + price[offer.currency] + '</span></a></li>\n');
+                + offer.retail_price + ' ' + currency[offer.currency] + '</span></a></li>\n');
               break;
 
           case 'free':
@@ -549,7 +524,7 @@ class JustWatchChrome {
                 + offer.monetization_type + ' ' + offer.presentation_type+'</span>  <span class="price">' 
                 + ((typeof offer.retail_price !== 'undefined')? offer.retail_price :'0')+''
                 + ( (typeof offer.currency !== 'undefined')?
-                    ((typeof price[offer.currency] !== 'undefined')? price[offer.currency] : offer.currency ) 
+                    ((typeof currency[offer.currency] !== 'undefined')? currency[offer.currency] : offer.currency ) 
                     : '-' ) 
                 +'</span></a></li>\n');
         }
@@ -567,8 +542,6 @@ class JustWatchChrome {
         }
       }
    
-      this.setPresentationTypesMenu(presentationTypes);
-
     } else {
       offersDiv.innerHTML = '<p class="message">NO OFFERS</p>';
     }
@@ -586,11 +559,13 @@ class JustWatchChrome {
   getWaitForSelector(){
     for (let hostname in supportedWeb) {
       if(location.hostname.match(hostname)){
-        console.log(hostname);
       
         if (typeof supportedWeb[hostname].waitForSelector != 'undefined'){
+          if(debug) console.log(hostname + ": waiting for [" + waitForSelector + "]");
+
           return supportedWeb[hostname].waitForSelector;
         } else {
+          if(debug) console.log(hostname + ": GO!");
           return '';
         }
         
@@ -600,243 +575,6 @@ class JustWatchChrome {
     return '';
   }
 
-}
-
-var providers = {
-  2: 'apple-itunes',
-  3: 'google-play-movies',
-  6: 'maxdome', // DE
-  7: 'vudu',
-  8: 'netflix',
-  9: 'amazon',
-  10: 'amazon-instant-video',
-  11: 'mubi',
-  12: 'crackle', // MX
-  15: 'hulu',
-  18: 'playstation',
-  19: 'netmovies', // BR
-  25: 'fandor',
-  27: 'hbo-now', // USA
-  31: 'hbo-go', // USA
-  34: 'epix',
-  35: 'rakuten-tv', //wuaki
-  //37: 'showtime', // USA
-  47: 'looke', // BR
-  52: 'cravetv', // CA
-  60: 'fandangonow',
-  62: 'atres-player',
-  63: 'filmin',
-  64: 'filmin-plus',
-  67: 'blim', // MX
-  68: 'microsoft-store',
-  73: 'tubi-tv',
-  //81: 'CINEMA',
-  84: 'u-next', // JP
-  85: 'dtv', // JP
-  86: 'gyao', // JP
-  100: 'guidedoc',
-  105: 'fandangonow',
-  118: 'hbo', //hboespana
-  119: 'amazon-prime-video',
-  140: 'cineplex', // CA
-  146: 'icitoutv', // CA
-  149: 'movistar-plus',
-  167: 'claro-video',
-  169: 'the-movie-network-go', // CA
-  182: 'hollywood-suite', // CA
-  188: 'youtube-red',
-  191: 'kanopy', // USA
-  192: 'youtube',
-  212: 'hoopla', // USA
-  215: 'syfy', // USA
-}
-
-var price = {
-  'EUR': '€',
-  'USD': '$',
-  'CAD': '$',
-  'JPY': '¥',
-  'MXN': '$',
-  'BRL': 'R$',
-}
-
-var supportedWeb = {
-  'imdb' : {
-    block: function(){ return document.getElementsByClassName('plot_summary_wrapper')[0]; },
-    title: function(){ return document.querySelectorAll('meta[name="title"]')[0].getAttribute('content'); },
-          // document.querySelectorAll('meta[property="og:title"]')[0].getAttribute('content');
-    year: function(){ 
-      var tmpYear = document.querySelectorAll('.titleBar *[itemprop="datePublished"]');
-      //         this.release_date = tmpYear[0].content;
-      return ( tmpYear.length > 0 )? tmpYear[0].content : null;
-     },
-    // no type
-  },
-  'rottentomatoes': {
-    // HAS LDJSON
-    block: function(){ return document.getElementById('watch-it-now'); },
-    title: function(){ return document.getElementsByTagName('h1')[0].innerText; },
-    year: function(){ return document.querySelectorAll('span.year')[0].innerText; },
-    // type from LDJSON
-  },
-  'tv.com': {
-    // HAS LDJSON
-    block: function(){ return document.getElementsByClassName('show_stats _clearfix')[0]; },
-    title: function(){ return document.getElementsByTagName('h1')[0].innerText; },
-    year: function(){ return document.getElementsByClassName('tagline')[0].innerText; }, // TODO: Not working on ended series
-  },
-  'sensacine.com/peliculas': {
-    type: "movie",
-    block: function(){ return document.getElementsByClassName('movie-card-overview')[0]; },
-    title: function(){ return document.getElementsByClassName('titlebar-title')[0].innerText; },
-    year: function(){ return document.getElementsByClassName('date')[0].innerText; },
-    // yearAlt: function(){ return document.getElementsByTagName('title')[0].innerText; },
-  },
-  'sensacine.com/series': {
-    type: "show",
-    block: function(){ return document.getElementsByClassName('entity-card-overview')[0]; },
-    title: function(){ return document.querySelectorAll('meta[property="og:title"]')[0].getAttribute('content'); },
-    year: function(){ return document.querySelectorAll('title')[0].innerText }, // TODO: Improve (there is a js object to look inside)
-  },
-  'filmaffinity.com': {
-    block: function(){ return document.getElementsByClassName('movie-info')[0]; },
-    title: function(){ return document.querySelectorAll('meta[property="og:title"]')[0].getAttribute('content'); },
-    year: function(){ return null; },
-  },
-/*
-  'fotogramas.es': {
-    block: function(){ return document.getElementsByClassName('ficha')[0]; },
-    title: function(){ return document.querySelectorAll('h1[itemprop="name"]')[0].innerText; },
-    year: function(){ return document.querySelectorAll('time[itemprop="dateCreated"]')[0].getAttribute('datetime'); },
-  },
-*/
-  'trakt.tv/movies': {
-    block: function(){ return document.getElementById('huckster-desktop-wrapper'); },
-    title: function(){ return document.querySelectorAll('meta[property="og:title"]')[0].getAttribute('content'); },
-    year: function(){ return document.querySelectorAll('h1 .year')[0].innerText; },
-    type: "movie",
-  },
-  'trakt.tv/shows': {
-    block: function(){ return document.getElementById('huckster-desktop-wrapper'); },
-    title: function(){ return document.querySelectorAll('meta[property="og:title"]')[0].getAttribute('content'); },
-    year: function(){ return document.querySelectorAll('h1 .year')[0].innerText; },
-    type: "show",
-  },
-  'metacritic.com/movie': {
-    // HAS LDJSON
-    block: function(){ return document.getElementsByClassName('esite_list')[0]; },
-    title: function(){ return document.querySelectorAll('meta[property="og:title"]')[0].getAttribute('content'); },
-    year: function(){ return null; },
-    type: "movie",
-  },
-  'metacritic.com/tv': {
-    block: function(){ return document.getElementsByClassName('esite_list')[0]; },
-    title: function(){ return document.querySelectorAll('meta[property="og:title"]')[0].getAttribute('content'); },
-    year: function(){ return document.querySelectorAll('.release_data .data')[0].innerText; },
-    type: "show",
-  },
-  'cinefilica.es': {
-    block: function(){ return document.querySelectorAll('title-primary-details-panel')[0]; },
-    title: function(){ return document.querySelectorAll('title')[0].innerText; },
-    year: function(){ return document.querySelectorAll('h1.title .year')[0].innerText; },
-  },
-  'ecartelera.com': {
-    block: function(){ return document.getElementById('scorebox'); },
-    title: function(){ return document.querySelectorAll('meta[property="og:title"]')[0].getAttribute('content'); },
-        //titleFull = document.querySelectorAll('span[itemprop="name"]')[0].innerText;
-    year: function(){ return document.querySelectorAll('head title')[0].innerText; },
-  },
-  'themoviedb.org/movie': {
-    block: function(){ return document.getElementsByClassName('header')[0]; },
-    title: function(){ return document.querySelectorAll('meta[property="og:title"]')[0].getAttribute('content');  },
-    year: function(){ return document.getElementsByClassName('release_date')[0].innerText; },
-    type: "movie",
-  },
-  'themoviedb.org/tv': {
-    block: function(){ return document.getElementsByClassName('header')[0]; },
-    title: function(){ return document.querySelectorAll('meta[property="og:title"]')[0].getAttribute('content');  },
-    year: function(){ return document.getElementsByClassName('release_date')[0].innerText; },    
-    type: "show", 
-  },
-  'thetvdb.com': {
-    block: function(){ return document.getElementById('series_basic_info'); },
-    title: function(){ return document.getElementById('series_title').innerText;  },
-    year: function(){ return document.querySelectorAll('#series_basic_info li span')[2].innerText; }, //TODO
-    type: "show",
-  },
-  'simkl.com/movies': {
-    block: function(){ return document.getElementsByClassName('SimklTVAboutBlockTitle')[0]; },
-    title: function(){ return document.querySelectorAll('h1[itemprop="name"]')[0].innerText; },
-    year:  function(){ return document.getElementsByClassName('detailYearInfo')[0].innerText; },    
-    type: "movie",
-  },
-  'simkl.com/tv': {
-    block: function(){ return document.getElementsByClassName('SimklTVAboutBlockTitle')[0]; },
-    title: function(){ return document.querySelectorAll('h1[itemprop="name"]')[0].innerText; },
-    year:  function(){ return document.getElementsByClassName('detailYearInfo')[0].innerText; },    
-    type: "show",
-  },
-  'criticker.com/film': {
-    block: function(){ return document.getElementById('fi_moreinfo'); },
-    title: function(){ return document.querySelectorAll('h1 [itemprop="name"]')[0].innerText; },
-    year:  function(){ return document.querySelectorAll('h1 [itemprop="datePublished"]')[0].innerText; },
-    type: "movie",
-  },
-  'criticker.com/tv': {
-    block: function(){ return document.getElementById('fi_moreinfo'); },
-    title: function(){ return document.querySelectorAll('h1 [itemprop="name"]')[0].innerText; },
-    year:  function(){ return document.querySelectorAll('h1 [itemprop="startDate"]')[0].innerText; },
-    type: "show",
-  },
-  'abc.es': {
-    block: function(){ return document.getElementsByClassName('datos-ficha')[0]; },
-    title: function(){ return document.querySelectorAll('meta[property="og:title"]')[0].getAttribute('content'); },
-    year:  function(){ return null; },
-    type: "movie",
-  },
-  'letterboxd.com': {
-    block: function(){ return document.getElementById('featured-film-header'); },
-    title: function(){ return document.querySelectorAll('meta[property="og:title"]')[0].getAttribute('content'); },
-    year:  function(){ return document.querySelectorAll('*[itemprop="datePublished"]')[0].innerText; },
-    type: "movie",
-  },
-  'tvtime.com': {
-    block: function(){ return document.getElementsByClassName('show-nav')[0]; },
-    title: function(){ return document.querySelectorAll('meta[property="og:title"]')[0].getAttribute('content'); },
-    year:  function(){ return document.querySelectorAll('meta[itemprop="startDate"]')[0].getAttribute('content'); },
-    type: "show",
-  },
-  'allmovie.com': {
-    block: function(){ return document.getElementsByClassName('affiliate-links')[0]; },
-    title: function(){ return document.querySelectorAll('meta[property="og:title"]')[0].getAttribute('content'); },
-    year:  function(){ return document.getElementsByClassName('release-year')[0].getAttribute('content'); },
-    type: "movie",
-  },
-  'trailers.apple.com': {
-    waitForSelector: 'h1.replaced',
-    block: function(){ return document.querySelectorAll('header#main-header')[0]; },
-    title: function(){ return document.querySelectorAll('h1.replaced')[0].innerText; },
-    year: function(){ return document.getElementsByClassName('release')[0].innerText },
-    type: "movie",
-  },
-
-  'netflix.com': {
-    waitForSelector: '.jawBoneContainer',
-    block: function(){ return document.querySelectorAll('.jawBoneContainer')[0]; },
-    title: function(){ return document.querySelectorAll('h1 .title-small img')[0].getAttribute('alt'); },
-    year: function(){ return document.getElementsByClassName('year')[0].innerText },
-  },
-  'primevideo.com': {
-    block: function(){ return document.querySelectorAll('.av-meta-info-wrapper')[0]; },
-    title: function(){ return document.querySelectorAll('h1')[0].innerText; },
-    year: function(){ return document.querySelectorAll('.av-badge-text[data-automation-id="release-year-badge"]')[0].innerText }, 
-  },
-  'hboespana.com': {
-    waitForSelector: 'h1[data-automation="title"]',
-    block: function(){ return document.querySelectorAll('.page > div')[0]; },
-    title: function(){ return document.querySelectorAll('h1')[0].innerText; },
-    year: function(){ return document.querySelectorAll('div[data-automation="meta_year"]')[0].innerText },     
-  }
 }
 
 var plugin;
@@ -854,11 +592,8 @@ document.body.onload = function(){
       var waitForSelector = plugin.getWaitForSelector();
 
       if (waitForSelector.length == 0){
-        console.log('NO wait');
           plugin.execute();
       } else {
-        console.log("waitng for [" + waitForSelector + "]");
-
         var checkExist = setInterval(function() {
           if (document.querySelectorAll(waitForSelector).length) {
             console.log("Exists! [" + waitForSelector + "]");
